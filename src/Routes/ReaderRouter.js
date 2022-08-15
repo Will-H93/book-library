bf6871ddf6ebf51e286770aa724f6322449d5ccb
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-08-15 21:08:54 +0100

    Reader tests pass

--- src/Routes/ReaderRouter.js
+++ src/Routes/ReaderRouter.js
@@ -4,5 +4,8 @@ const readerController = require("../controllers/ReaderConroller");
 const readerRouter = new Router();
 
 readerRouter.post("/", readerController.create);
-
+readerRouter.get("/", readerController.read);
+readerRouter.get("/:id", readerController.readId);
+readerRouter.patch("/:id", readerController.update);
+readerRouter.delete("/:id", readerController.delete);
 module.exports = readerRouter;
--- src/controllers/ReaderConroller.js
+++ src/controllers/ReaderConroller.js
@@ -3,7 +3,50 @@ const { Reader } = require("../models");
 exports.create = async (req, res) => {
   const dbReader = await Reader.create(req.body);
   if (!dbReader) {
-    return res.status(500).json({ error: "Reader not created" });
+    return res.status(404).json({ error: "Reader not created" });
   }
   return res.status(201).json(dbReader);
+};
+
+exports.read = async (req, res) => {
+  const dbReader = await Reader.findAll();
+  if (!dbReader) {
+    return res.status(404).json({ error: "Reader not found" });
+  }
+  return res.status(200).json(dbReader);
+};
+
+exports.readId = async (req, res) => {
+  const readerId = req.params.id;
+  const dbReader = await Reader.findByPk(readerId);
+
+  if (!dbReader) {
+    return res.status(404).json({ error: "The reader could not be found." });
+  }
+  return res.status(200).json(dbReader);
+};
+
+exports.update = async (req, res) => {
+  const readerId = req.params.id;
+  const updateData = req.body;
+
+  const [updatedRows] = await Reader.update(updateData, {
+    where: { id: readerId },
+  });
+
+  if (!updatedRows) {
+    return res.status(404).json({ error: "The reader could not be found." });
+  }
+  return res.status(200).json({ result: "Reader Updated" });
+};
+
+exports.delete = async (req, res) => {
+  const readerId = req.params.id;
+
+  const deletedRows = await Reader.destroy({ where: { id: readerId } });
+
+  if (!deletedRows) {
+    return res.status(404).json({ error: "The reader could not be found." });
+  }
+  return res.status(204).json({ result: "Reader Deleted" });
 };
