11a4b178ebcd650bc2b66c7096c45fbe51580ec7
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-08-21 23:09:28 +0100

    added reader validation

--- src/models/reader.js
+++ src/models/reader.js
@@ -1,8 +1,25 @@
 module.exports = (connection, DataTypes) => {
   const schema = {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true,
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true,
+        is: (/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/)
+      }
+    }
   };
 
   const ReaderModel = connection.define("Reader", schema);
